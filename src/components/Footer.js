a8d8dec688ed8224dbe835c34ebb37ad13c813a6
Dhruv Hetal Kumar Vyas <dxv230008@utdallas.edu>
2024-11-12 16:53:19 -0600

    Fix ESLint warnings and add missing Babel plugin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const FooterContainer = styled.footer`
   background-color: #0a0a0a;
